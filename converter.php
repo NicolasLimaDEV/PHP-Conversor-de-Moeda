2f586f0dc12f143f50646f5bf567858db2145e2a
nicolaslimadev <nicolaslima.dev.web@gmail.com>
2025-11-21 16:18:08 -0300

    feat: Consumindo API do Banco Central do Brasil, utlizando a biblioteca INTL PHP para a formatação da moeda.

--- converter.php
+++ converter.php
@@ -1,3 +1,24 @@
+<?php 
+   //Consumindo API do Banco Central
+   $inicio = date("m-d-Y", strtotime(" -7 days "));
+   $final = date("m-d-Y");
+   $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''.$inicio.'\'&@dataFinalCotacao=\''.$final.'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+   $dados = json_decode(file_get_contents($url), true);
+
+   $cotacao = $dados["value"][0]["cotacaoCompra"];
+   $diaHoje = $dados["value"][0]["dataHoraCotacao"];
+
+   //Pegando dados do formulário 
+   $real = $_REQUEST["moeda"];
+
+   //Calculo do dolar
+   $dolar = $real / $cotacao;
+
+   $padrao = numfmt_create("pt-BR", NumberFormatter::CURRENCY);
+   
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -13,7 +34,7 @@
       <div class="card">
          <h1>Resultado</h1>
 
-         <h3>Seus <span class="dinheiro">R$ 150,00</span> equivalem a <span class="dinheiro">US$ 100,00</span></h3>
+         <h3>Seus <span class="dinheiro"> <?= numfmt_format_currency($padrao, $real, "BRL") ?></span> equivalem a <span class="dinheiro"><?= numfmt_format_currency($padrao, $dolar, "USD") ?></span></h3>
          <p>Cotação obtida diretamente do site do Banco do Brasil.</p>
 
          <a class="btn-voltar" href="index.php">Voltar</a>
